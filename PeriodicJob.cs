592f21a52c00107de26cff738d1f68627e9831c5
Wolf Wolfswinkel <>
2020-12-04 23:27:09 +0100

    Initial revision

---
+++ PeriodicJob.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class PeriodicJob
+{
+    private readonly int _intervalSeconds;
+    private int _runCount;
+
+    public PeriodicJob(int intervalSeconds)
+    {
+        _intervalSeconds = intervalSeconds;
+    }
+    public async Task Run(CancellationToken ct)
+    {
+        try
+        {
+            while ( !ct.IsCancellationRequested )
+            {
+                ++_runCount;
+                Console.WriteLine($"Periodic job run {_runCount}");
+                await Task.Delay(TimeSpan.FromSeconds(_intervalSeconds), ct);
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            // Consider cancellation as normal exit
+            return;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace csharp_async_demo
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Main() enter");
+
+                var runCts = new CancellationTokenSource();
+
+                Console.CancelKeyPress += (sender, args) => {
+                    runCts.Cancel();
+                    args.Cancel = true; // Prevent event propagation; we should get normal program exit
+                };
+
+
+                var periodicJob = new PeriodicJob(10);
+
+                Console.WriteLine("Starting periodicJob");
+
+                var periodicJobTask = periodicJob.Run(runCts.Token);
+
+                Console.WriteLine("Waiting for Ctrl-C and all tasks to finish");
+                await periodicJobTask;
+
+                Console.WriteLine("Main() end");
+                return 0;
+            }
+            catch ( Exception e )
+            {
+                Console.WriteLine($"Main caught {e}");
+                return 1;
+            }
+        }
+    }
+}
